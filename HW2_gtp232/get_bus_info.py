d25bf9ae8e945c0fdfd1313c1c22148e26f6822a
Geoff Perrin <gtp232@compute.cusp.nyu.edu>
2016-09-20 20:50:44 -0400

    updating get_bus_info.py

--- HW2_gtp232/get_bus_info.py
+++ HW2_gtp232/get_bus_info.py
@@ -26,9 +26,12 @@ busses_clean = [[u'Latitude',u'Longitude',u'Stop Name',u'Stop Status']]
 for i in range(len(busses)):
   lat = busses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
   lon = busses[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-  stop = busses[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
-  status = busses[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
-    
+  if (busses[i]['MonitoredVehicleJourney']['OnwardCalls']=={}):
+	stop = "N/A"
+	status = "N/A"
+  else:
+	stop = busses[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+	status = busses[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance'] 
   bus_new = [lat,lon,stop,status]
   busses_clean.append(bus_new)
  
